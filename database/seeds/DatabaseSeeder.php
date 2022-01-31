3de4cc58faa6bbe1a948f6efb82fc411dc600c82
casalazar562 <43974698+casalazar562@users.noreply.github.com>
2022-01-31 11:07:34 -0500

    commit prueba seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,8 +1,17 @@
 <?php
 
-Class DatabaseSeeder extends \Illuminate\Database\Seeder
-{
-    public function run(){
+use App\Models\Zonas;
+use Illuminate\Support\Facades\DB;
 
+class DatabaseSeeder extends \Illuminate\Database\Seeder
+{
+    public function run()
+    {
+        // $this->call(UserSeeder::class);
+        Zonas::factory(10)->create();
+        $path = 'database/seeds/sql/departamentos.sql';
+        DB::unprepared(file_get_contents($path));
+        $path = 'database/seeds/sql/ciudades.sql';
+        DB::unprepared(file_get_contents($path));
     }
 }
