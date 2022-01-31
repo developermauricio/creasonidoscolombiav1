af9b1c54f75ac1c110df5c1fcc03bf93dc8eea99
developermauricio <70330439+developermauricio@users.noreply.github.com>
2022-01-31 11:01:37 -0500

    ajuste composer

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,25 +1,8 @@
 <?php
 
-namespace Database\Seeders;
-
-use App\Models\Zonas;
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-
-class DatabaseSeeder extends Seeder
+Class DatabaseSeeder extends \Illuminate\Database\Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
-        Zonas::factory(10)->create();
-        $path = 'database/seeds/sql/departamentos.sql';
-        DB::unprepared(file_get_contents($path));
-        $path = 'database/seeds/sql/ciudades.sql';
-        DB::unprepared(file_get_contents($path));
+    public function run(){
+
     }
 }
