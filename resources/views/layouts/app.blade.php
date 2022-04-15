2beb70ad7cb012f524d544acf8d78dc41c31975c
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2022-04-15 17:22:41 +0000

    ajustes movil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    {{--    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">--}}
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
     {{--    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">--}}
     {{--    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">--}}
     <meta name="author" content="CREASONIDOS COLOMBIA">
@@ -61,7 +61,7 @@
 <div class="app-content content" id="app">
     <div class="content-overlay"></div>
     <div class="header-navbar-shadow"></div>
-    <div class="content-wrapper container-xxl p-0">
+    <div class="content-wrapper p-0">
         <div class="content-header row">
         </div>
         <div class="content-body">
