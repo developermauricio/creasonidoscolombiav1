4ae14f6817d214888c20b6f42aeb86dec02dd19f
developermauricio <70330439+developermauricio@users.noreply.github.com>
2022-04-15 16:10:42 -0500

    se añadio el recaptcha

--- resources/views/aspirant/account.blade.php
+++ resources/views/aspirant/account.blade.php
@@ -51,6 +51,40 @@
             gtag('config', 'G-PXCL7MRG4N');
         </script>
     @endif
+    <script type="text/javascript">
+
+        function callbackThen(response){
+
+            // read HTTP status
+
+            console.log(response.status);
+
+            // read Promise object
+
+            response.json().then(function(data){
+
+                console.log(data);
+
+            });
+
+        }
+
+        function callbackCatch(error){
+
+            console.error('Error:', error)
+
+        }
+
+    </script>
+
+    {!! htmlScriptTagJsApi([
+
+    'callback_then' => 'callbackThen',
+
+    'callback_catch' => 'callbackCatch'
+
+    ]) !!}
+
 </head>
 <!-- END: Head-->
 
