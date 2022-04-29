3d7081237fe6e40225f706921807b9cd46b6e8ff
developermauricio <70330439+developermauricio@users.noreply.github.com>
2022-04-29 09:46:34 -0500

    Ajustes en reportes

--- resources/views/reports/dashboard.blade.php
+++ resources/views/reports/dashboard.blade.php
@@ -3,6 +3,6 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <h1>Hola administrador</h1>
+    <iframe width="600" height="1072" src="https://datastudio.google.com/embed/reporting/a5409061-e062-419e-9aff-cd6edaabf7ee/page/p_2psbndu6rc" frameborder="0" style="border:0; width: 100% !important;" allowfullscreen></iframe>
 @endsection
 
